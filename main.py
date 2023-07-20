0e738902c013701920c38247e4b3996af5a5cfed
Jaqobel <me.jakub.gil@gmail.com>
2023-07-20 17:04:42 +0200

    Adding file to insert API KEY and way to change amount of generated images

--- main.py
+++ main.py
@@ -20,23 +20,39 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-
+IMG_AMMOUT = 1
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
+    global IMG_AMMOUT
+
+    if message.content.startswith('!img_amount'):
+        try:
+            int(message.content[12:])
+
+            if int(message.content[12:]) <= 0:
+                raise TypeError
+
+        except:
+            await message.channel.send(f"Ammout of creating images has to be an integer bigger than 0, you entered: \"{message.content[12:]}\"")
+        finally:
+            IMG_AMMOUT = int(message.content[12:])
+            await message.channel.send(f"Ammout of creating images has been succesfully saved \nCurrent number of images: {IMG_AMMOUT}")
+
     if message.content.startswith('!create_img'):
         await message.channel.send(f"Generating an image for you input: \"{message.content[12:]}\"")
         print(message.content[12:])
         image = openai.Image.create(
         prompt=f"{message.content[12:]}",
-        n=1,
+        n=IMG_AMMOUT,
         size="1024x1024"
         )
-
-        await message.channel.send(image["data"][0]['url'])
+        for i in range(IMG_AMMOUT):
+            await message.channel.send(f"Picture no.{i}")
+            await message.channel.send(image["data"][i]['url'])
 
     if message.content.startswith('!answer'):
         await message.channel.send(f"Generating an answer for you question: \"{message.content[8:]}\"")
@@ -49,6 +65,6 @@ async def on_message(message):
         )
 
         await message.channel.send(answer.choices[0].message.content)
-    
+
 
 client.run(TOKEN)
