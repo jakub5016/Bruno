0c28bab4872508ec81b57474e8cd8d8c0cfb7643
Jaqobel <me.jakub.gil@gmail.com>
2023-07-20 16:48:05 +0200

    Creating answers based on chat gpt

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ with open('OPENAI_API_KEY.txt', 'r') as file:
     data = file.readline().replace('\n', '')
     OPENAI_API_KEY = data[4:]
 
+openai.api_key = OPENAI_API_KEY
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -26,7 +28,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith('!create_img'):
-        openai.api_key = OPENAI_API_KEY
+        await message.channel.send(f"Generating an image for you input: \"{message.content[12:]}\"")
         print(message.content[12:])
         image = openai.Image.create(
         prompt=f"{message.content[12:]}",
@@ -36,5 +38,17 @@ async def on_message(message):
 
         await message.channel.send(image["data"][0]['url'])
 
+    if message.content.startswith('!answer'):
+        await message.channel.send(f"Generating an answer for you question: \"{message.content[8:]}\"")
+        print(message.content[8:])
+        answer = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": f"{message.content[8:]}"}
+        ]
+        )
+
+        await message.channel.send(answer.choices[0].message.content)
+    
 
 client.run(TOKEN)
